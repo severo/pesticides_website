4f95a9c909dc2ca22b26fae6839de748c22e5e0f
Sylvain Lesage <severo@rednegra.net>
2019-02-19 19:25:06 +0100

    Load a CSV file and show the number of rows (not tested locally)

--- src/_javascript/main.js
+++ src/_javascript/main.js
@@ -1,5 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+import {csv} from 'd3-fetch';
+
+const url =
+  'https://raw.githubusercontent.com/severo/data_brazil/master/data_by_municipality_for_maps.csv';
+
+csv(url).then(data => {
   console.log(
     'A comment in the console to test JavaScript in https://github.com/severo/pesticides_website!'
+  );
+  console.log(
+    'The CSV file has been loaded, it contains ' + data.length + ' rows'
   );
 });
