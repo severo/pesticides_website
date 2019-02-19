1509416dd38a7c948f6869fc75035427d6998f21
Sylvain Lesage <severo@rednegra.net>
2019-02-19 18:02:53 +0100

    Lint against ES2015

--- src/_javascript/main.js
+++ src/_javascript/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log(
-    'A comment in the console to test JavaScript in https://github.com/severo/pesticides_website!',
+    'A comment in the console to test JavaScript in https://github.com/severo/pesticides_website!'
   );
 });
