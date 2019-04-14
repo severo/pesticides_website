aa899807e51f8361895c044c828c79c24dfa4262
Sylvain Lesage <severo@rednegra.net>
2019-04-14 17:41:11 +0200

    fix: simplify breadcrumb and change text

--- src/_javascript/breadcrumb/index.js
+++ src/_javascript/breadcrumb/index.js
@@ -21,34 +21,17 @@ export function makeBreadcrumb(parent, dispatcher, state) {
 
 function makeBrazil(parent) {
   parent.html(null);
-  parent
-    .append('ul')
-    .append('li')
-    .classed('is-active', true)
-    .classed('is-hidden', true)
-    .append('a')
-    .attr('href', '#')
-    .attr('aria-current', 'page')
-    .text('{{breadcrumb.brazil}}');
 }
 
 function makeMun(parent, dispatcher, data, mun) {
   parent.html(null);
-  const ul = parent.append('ul');
-  ul.append('li')
+  parent
     .append('a')
     .attr('href', '#')
-    .text('{{breadcrumb.brazil}}')
+    .text('{{breadcrumb.goto-brazil}}')
     .on('click', (ft, element) => {
-      // invoke callbacks
       dispatcher.call('breadcrumb-click-brazil', null, data);
     });
-  ul.append('li')
-    .classed('is-active', true)
-    .append('a')
-    .attr('href', '#')
-    .attr('aria-current', 'page')
-    .text(mun.properties.name);
 }
 
 function startLoading(element) {
